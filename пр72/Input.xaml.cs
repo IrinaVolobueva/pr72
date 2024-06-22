6fb7ffdbf9f3a20d847502edb616c9eba5de2d05
IrinaVolobueva <145708238+IrinaVolobueva@users.noreply.github.com>
2024-06-22 14:46:20 +0300

    Add files via upload

---
+++ пр72/Input.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace пр72
+{
+    /// <summary>
+    /// Логика взаимодействия для Input.xaml
+    /// </summary>
+    public partial class Input : Window
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+        public bool Perimeter { get; set; }
+        public bool Area { get; set; }
+        public bool Median { get; set; }
+
+        public Input()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (Double.TryParse(SideAtext.Text, out double sideA) && Double.TryParse(SideBtext.Text, out double sideB) && Double.TryParse(SideCtext.Text, out double sideC))
+            {
+                SideA = sideA;
+                SideB = sideB;
+                SideC = sideC;
+                if (sideA > sideB + sideC || sideB > sideA + sideC || sideC > sideA + sideB)
+                {
+                    MessageBox.Show("The sum of the two sides cannot be less than the length of the third side", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    SideCtext.Clear();
+                    SideCtext.Clear();
+                    SideCtext.Clear();
+                    PerimeterCheckBox.IsChecked = false;
+                    AreaCheckBox.IsChecked = false;
+                    MedianCheckBox.IsChecked = false;
+                }
+                else if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+                {
+                    MessageBox.Show("The value cannot be negative or equal to zero.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    SideAtext.Clear();
+                    SideBtext.Clear();
+                    SideCtext.Clear();
+                    PerimeterCheckBox.IsChecked = false;
+                    AreaCheckBox.IsChecked = false;
+                    MedianCheckBox.IsChecked = false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Incorrect input of the sides of the triangle.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                SideAtext.Clear();
+                SideBtext.Clear();
+                SideCtext.Clear();
+                PerimeterCheckBox.IsChecked = false;
+                AreaCheckBox.IsChecked = false;
+                MedianCheckBox.IsChecked = false;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Perimeter = PerimeterCheckBox.IsChecked ?? false;
+            Area = AreaCheckBox.IsChecked ?? false;
+            Median = MedianCheckBox.IsChecked ?? false;
+
+            DialogResult = true;
+        }
+    }
+}
---
+++ пр72/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace пр72
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private double sideA, sideB, sideC;
+
+        private bool perimeter, area, median;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Input input = new Input();
+            if (input.ShowDialog() == true)
+            {
+                sideA = input.SideA;
+                sideB = input.SideB;
+                sideC = input.SideC;
+                perimeter = input.Perimeter;
+                area = input.Area;
+                median = input.Median;
+            }
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (sideA == 0 && sideB == 0 && sideC == 0)
+            {
+                MessageBox.Show("Please input the sides of the triangle first.");
+                return;
+            }
+
+            Result resultWindow = new Result();
+            resultWindow.PerimeterTextBox.Text = perimeter ? Perimeter().ToString() : "Not calculated";
+            resultWindow.AreaTextBox.Text = area ? Area().ToString() : "Not calculated";
+            resultWindow.MedianTextBox.Text = median ? Median().ToString() : "Not calculated";
+
+            resultWindow.ShowDialog();
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        /// <summary>
+        /// Method Perimeter() to calculate the perimeter
+        /// </summary>
+        /// <returns></returns>
+        private double Perimeter()
+        {
+            return Math.Round(sideA + sideB + sideC, 3);
+        }
+        /// <summary>
+        /// Method Area() to calculate the area
+        /// </summary>
+        /// <returns></returns>
+        private double Area()
+        {
+            double half_Perimeter = Perimeter() / 2;
+            return Math.Round(Math.Sqrt(half_Perimeter * (half_Perimeter - sideA) * (half_Perimeter - sideB) * (half_Perimeter - sideC)), 3);
+        }
+        /// <summary>
+        /// Method Median() to calculate the median
+        /// </summary>
+        /// <returns></returns>
+        private double Median()
+        {
+            return Math.Round(Math.Sqrt(2 * (sideA * sideA + sideB * sideB) - sideC * sideC) / 2, 3);
+        }
+
+    }
+}
